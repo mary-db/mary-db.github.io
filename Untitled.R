cf312b832e53119dd3b692599942524d735c8716
Mary de Boer <medeboer@gmail.com>
2025-05-19 08:39:29 -0400

    Deploy updated site: move files to repo root for GH Pages

---
+++ Untitled.R
@@ -0,0 +1,13 @@
+## Start version controlling it
+usethis::use_git()
+
+## Use the gh-pages branch in order for
+## GitHub pages https://pages.github.com/ to
+## host our website.
+usethis::git_default_branch_rename(to = "gh-pages")
+
+## Create a .nojekyll file
+writeLines("", here::here(".nojekyll"))
+
+## Share it via GitHub with the world
+usethis::use_github()
